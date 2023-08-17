3e5e72a08008366c4bdf91ac8b515e68b493baa6
Maryanne Wambugu <2wambugu.marie@gmail.com>
2023-08-17 16:19:53 +0300

    Completed

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -8,7 +8,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant_by_id
     render json: plant
   end
 
@@ -18,9 +18,38 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update 
+    plant = find_plant_by_id
+    if plant
+      plant.update(params.permit(:is_in_stock))
+      render json: plant
+    else 
+      render_not_found
+    end
+  end
+
+  def destroy
+    plant = find_plant_by_id
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render_not_found
+    end
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant_by_id
+    Plant.find_by(id: params[:id])
+  end
+
+  def render_not_found
+    render json: {error: "Plant not found"}, status: :not_found
+  end
+   
 end
